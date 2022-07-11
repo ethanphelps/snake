04d9b10f78e2e315cb63783cf9c1840ea48c6d5a
ethanphelps <ethanrphelpsapps@gmail.com>
2022-07-10 23:02:51 -0500

    small changes

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # snake
 This is a snake implementation I'm making for fun to get experience with Typescript, Web Components and HTML Canvas. It's built using Lit Element web components and has a simple typescript game "engine" that runs and draws the game onto the HTML Canvas.
 
+A live version of the game is deployed at [ethanphelps.dev/snake](https://ethanphelps.dev/snake)
+
 To install and run the game, copy the repo url, git clone it, and then run the following in your terminal:
 ```
 cd snake && npm install
---
+++ webpack.prod.js
@@ -0,0 +1,83 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = (env, argv) => ({
+  mode: "production",
+  entry: {
+    app: "./src/index.ts",
+  },
+  devtool: argv.mode === "production" ? "none" : "inline-source-map",
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        include: path.resolve(__dirname, "src"),
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.s[a|c]ss$/,
+        use: [
+          { loader: "lit-scss-loader" },
+          {
+            loader: "extract-loader",
+            // options: {
+            //   esModule: false,
+            // },
+          },
+          { loader: "css-loader",
+            options: {
+              esModule: false,
+            }
+          },
+          { loader: "sass-loader" },
+        ],
+      },
+      {
+        test: /\.(png|gif|jpg|cur)$/i,
+        loader: "url-loader",
+        // type: 'asset/resource',
+        options: { limit: 8192 },
+      },
+      {
+        test: /\.woff2(\?v=[0-9]\.[0-9]\.[0-9])?$/i,
+        loader: "url-loader",
+        options: { limit: 10000, mimetype: "application/font-woff2" },
+      },
+      {
+        test: /\.woff(\?v=[0-9]\.[0-9]\.[0-9])?$/i,
+        loader: "url-loader",
+        options: { limit: 10000, mimetype: "application/font-woff" },
+      },
+      {
+        test: /\.(ttf|eot|svg|otf)(\?v=[0-9]\.[0-9]\.[0-9])?$/i,
+        loader: "file-loader",
+        options: {
+          esModule: false,
+        }
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+});
