7c7f65207f08fdc35e0f1cdb8e10030d36d0a73b
ethanphelps <ethanrphelpsapps@gmail.com>
2022-07-17 17:19:05 -0500

    wavy animation achieved

--- src/components/high-score/high-score.component.ts
+++ src/components/high-score/high-score.component.ts
@@ -6,16 +6,39 @@ import styles from './high-score.component.scss'
 export class HighScore extends LitElement {
   static styles = [styles]
 
+ highScore = html`
+    <span class="layer">
+      <!-- <span class="word"> -->
+        <span>H</span>
+        <span>I</span>
+        <span>G</span>
+        <span class="end-word">H</span>
+      <!-- </span> -->
+      <!-- <span class="word"> -->
+        <span>S</span>
+        <span>C</span>
+        <span>O</span>
+        <span>R</span>
+        <span>E</span>
+      <!-- </span> -->
+    </span>`;
+
   render(): TemplateResult {
     return html`
       <div class="container">
         <h1>
+          <!-- <span>HIGH SCORE</span>  -->
+          ${this.highScore}
+          ${this.highScore}
+          ${this.highScore}
+          ${this.highScore}
+          ${this.highScore}
+          ${this.highScore}
+          <!-- <span>HIGH SCORE</span> 
           <span>HIGH SCORE</span> 
           <span>HIGH SCORE</span> 
           <span>HIGH SCORE</span> 
-          <span>HIGH SCORE</span> 
-          <span>HIGH SCORE</span> 
-          <span>HIGH SCORE</span> 
+          <span>HIGH SCORE</span>  -->
         </h1>
       </div>
     `
